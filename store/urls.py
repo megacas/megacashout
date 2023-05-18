4558ded94ea9be270085ff11808814031ae205c7
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-05-18 16:04:34 +0000

    changes

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home,name="home"),
     path('category/<slug:category_slug>', views.category_list, name='category_list'),
     path('random_names/', views.trial, name='random_name'),
+    path('download/', views.download_csv, name='download'),
 ]
--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import *
 from payment.models import *
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
 from .context_processors import random_name
 @login_required
 def home(request):
@@ -16,3 +16,18 @@ def category_list(request, category_slug):
 def trial(request):
     
     return JsonResponse(random_name(request))
+
+from django.http import FileResponse
+import os
+
+def download_csv(request):
+    file_path = '/static/output.csv'  # Replace with the actual path to the exported CSV file
+
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as file:
+            response = FileResponse(file)
+            response['Content-Type'] = 'text/csv'
+            response['Content-Disposition'] = 'attachment; filename="output.csv"'
+            return response
+    else:
+        return HttpResponse('File not found.')
