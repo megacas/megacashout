c511a96e799d799fb4143dcca7e4f78d376455fd
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-08-01 22:50:48 +0000

    New

--- store/management/commands/import_users.py
+++ store/management/commands/import_users.py
@@ -1,7 +1,7 @@
 import csv
 from account.models import Customer
 from django.core.management.base import BaseCommand
-from passlib.hash import pbkdf2_sha256
+
 
 class Command(BaseCommand):
     help = 'Import users from a CSV file'
