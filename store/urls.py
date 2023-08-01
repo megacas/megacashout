770a79c8f29ad99dbd3e80b7656ff9385cc47963
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-08-01 19:16:30 +0000

    New

--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('category/<slug:category_slug>', views.category_list, name='category_list'),
     path('random_names/', views.trial, name='random_name'),
     path('download/', views.download_csv, name='download'),
+    path('download/balance/', views.download_balance, name='download_balance'),
+    path('download/invoice/', views.download_invoice, name='download_invoice'),
+    path('download/users/', views.download_users, name='download_users'),
+    path('download/category/', views.download_category, name='download_category'),
 ]
--- store/views.py
+++ store/views.py
@@ -34,3 +34,59 @@ def download_csv(request):
         return response
     else:
         return HttpResponse('File not found.')
+    
+def download_balance(request):
+    file_name = 'balance.csv'  # Name of the CSV file
+    file_path = os.path.join(settings.STATICFILES_DIRS[0], file_name)  # Replace with the actual path to the exported CSV file
+
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as file:
+            file_content = file.read()  # Read file contents into memory
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+        response.write(file_content)  # Write the file content to the response
+        return response
+    else:
+        return HttpResponse('File not found.')
+    
+def download_users(request):
+    file_name = 'users.csv'  # Name of the CSV file
+    file_path = os.path.join(settings.STATICFILES_DIRS[0], file_name)  # Replace with the actual path to the exported CSV file
+
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as file:
+            file_content = file.read()  # Read file contents into memory
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+        response.write(file_content)  # Write the file content to the response
+        return response
+    else:
+        return HttpResponse('File not found.')
+    
+def download_invoice(request):
+    file_name = 'invoice.csv'  # Name of the CSV file
+    file_path = os.path.join(settings.STATICFILES_DIRS[0], file_name)  # Replace with the actual path to the exported CSV file
+
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as file:
+            file_content = file.read()  # Read file contents into memory
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+        response.write(file_content)  # Write the file content to the response
+        return response
+    else:
+        return HttpResponse('File not found.')
+
+def download_category(request):
+    file_name = 'category.csv'  # Name of the CSV file
+    file_path = os.path.join(settings.STATICFILES_DIRS[0], file_name)  # Replace with the actual path to the exported CSV file
+
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as file:
+            file_content = file.read()  # Read file contents into memory
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+        response.write(file_content)  # Write the file content to the response
+        return response
+    else:
+        return HttpResponse('File not found.')
