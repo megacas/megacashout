fdffaa21072a32aba6c2b460e992791092873b48
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-08-17 21:43:37 +0000

    Update admin.py

--- account/admin.py
+++ account/admin.py
@@ -8,5 +8,5 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ('user_name', 'email', 'is_active')
     list_filter = ('is_active',)  # Make sure to use a tuple with a comma at the end
     
-    readonly_fields = ('email',)  # Make sure to use a tuple with a comma at the end
+    #readonly_fields = ('email',)  # Make sure to use a tuple with a comma at the end
     search_fields = ('user_name','email',)
