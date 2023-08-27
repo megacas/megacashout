3513dc148ef1c2b5c6b4e699f2bbd7fee9c645a8
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-08-27 11:12:34 +0000

    new

--- payment/admin.py
+++ payment/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import *
 admin.site.register(Invoice)
-admin.site.register(Balance)
+class BalanceAdmin(admin.ModelAdmin):
+    list_display = ('created_by','balance')
+    search_fields = ('created_by',)
+    list_editable = ('balance',)
+    
+
+admin.site.register(Balance, BalanceAdmin)
