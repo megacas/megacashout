9399f96c4bd31aa140369be6e1de04f1f1146594
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-08-27 15:32:54 +0000

    Update

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-
 from . import views
-from .context_processors import random_name
 urlpatterns = [
-    path('', views.maintenance,name="home"),
-    #path('category/<slug:category_slug>', views.category_list, name='category_list'),
+    path('', views.home,name="home"),
+    path('category/<slug:category_slug>', views.category_list, name='category_list'),
     path('random_names/', views.trial, name='random_name'),
     path('download/', views.download_csv, name='download'),
     path('download/balance/', views.download_balance, name='download_balance'),
